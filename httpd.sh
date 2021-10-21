34fff67a0c19c2fb4af79d58e39c9611d6b7b29f
unknown <amkk0425@gmail.com>
2021-10-21 15:33:46 +0900

    five

--- httpd.sh
+++ httpd.sh
@@ -6,7 +6,7 @@ sudo yum install -y gcc gcc-c++ wget expat-devel
 sudo wget -P /web https://downloads.apache.org/apr/apr-1.7.0.tar.gz
 sudo wget -P /web https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo wget -P /web https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
-wget https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
+sudo wget -P /web https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
 sudo cd /web
 sudo tar xvfz apr-1.7.0.tar.gz
 sudo tar xvfz apr-util-1.6.1.tar.gz
