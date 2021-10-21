20ff1bb6e6ddddac6340456275d853d579d640ac
unknown <amkk0425@gmail.com>
2021-10-21 15:39:14 +0900

    six

--- httpd.sh
+++ httpd.sh
@@ -8,10 +8,10 @@ sudo wget -P /web https://downloads.apache.org/apr/apr-util-1.6.1.tar.gz
 sudo wget -P /web https://downloads.apache.org/httpd/httpd-2.4.51.tar.gz
 sudo wget -P /web https://sourceforge.net/projects/pcre/files/pcre/8.45/pcre-8.45.tar.gz --no-check-certificate
 sudo cd /web
-sudo tar xvfz apr-1.7.0.tar.gz
-sudo tar xvfz apr-util-1.6.1.tar.gz
-sudo tar xvfz httpd-2.4.51.tar.gz
-sudo tar xvfz pcre-8.45.tar.gz
+sudo tar xvfz /web/apr-1.7.0.tar.gz
+sudo tar xvfz /web/apr-util-1.6.1.tar.gz
+sudo tar xvfz /web/httpd-2.4.51.tar.gz
+sudo tar xvfz /web/pcre-8.45.tar.gz
 sudo cd /web/apr-1.7.0 ; sudo ./configure --prefix=/web/apr ; sudo make && sudo make install
 sudo cd /web/apr-util-1.6.1 ; sudo ./configure --prefix=/web/aprutil --with-apr=/web/apr ; sudo make && sudo make install
 sudo cd /web/pcre-8.45 ; ./configure --prefix=/web/pcre ; sudo make && sudo make install
